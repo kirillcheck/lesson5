af43c521841e19dd9701d107bf1c082a96927c08
Кирилл <shakhov9200@mail.ru>
2019-03-10 23:07:39 +0300

    Два задания

--- part2/js/script.js
+++ part2/js/script.js
@@ -1,84 +1,23 @@
-// кнопка расчета бюджета
-let button = document.getElementById('start');
-console.log(button);
-// кнопка расчета бюджета
+let button = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalexpensesValue = document.querySelectorAll('.optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-
-//Получить все блоки в правой части программы через классы
-let budgetValue = document.getElementsByClassName('budget-value');
-console.log(budgetValue[0]);
-
-let daybudgetValue = document.getElementsByClassName('daybudget-value');
- console.log(daybudgetValue[0]);
-
- let levelValue = document.getElementsByClassName('level-value');
- console.log(levelValue[0]);
-
- let expensesValue = document.getElementsByClassName('expenses-value');
- console.log(expensesValue[0]);
-
- let incomeValue = document.getElementsByClassName('income-value');
- console.log(incomeValue[0]);
-
- let monthSavingsValue = document.getElementsByClassName('monthsavings-value');
- console.log(monthSavingsValue[0]);
-
- let yearSavingsValue = document.getElementsByClassName('yearsavings-value');
- console.log(yearSavingsValue[0]);
-
- 
-
-
-//Получить все блоки в правой части программы через классы
-
-
-
-
-//  Получение поля(input) c обязательными расходами через класс expenses-item
-let expensesInput = document.getElementsByClassName('expenses-item');
-console.log(expensesInput[0]);
-console.log(expensesInput[1]);
-console.log(expensesInput[2]);
-console.log(expensesInput[3]);
-//  Получение поля(input) c обязательными расходами через класс expenses-item
-
-
-//   Получить кнопки
-let buttonApprove = document.getElementsByTagName('button');
-console.log(buttonApprove[0]);
-console.log(buttonApprove[1]);
-console.log(buttonApprove[2]);
-//   Получить кнопки
-
-
-//   Получить поля для ввода необязательных расходов (optionalexpenses-item) при помощи querySelectorAll
-let optionalexpenses = document.querySelectorAll('.optionalexpenses-item');
-console.log(optionalexpenses[0]);
-console.log(optionalexpenses[1]);
-console.log(optionalexpenses[2]);
-//   Получить поля для ввода необязательных расходов (optionalexpenses-item) при помощи querySelectorAll
-
-
-//   Получить оставшиеся поля через querySelector (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
-let yearInput = document.querySelector('.year-value');
-console.log(yearInput);
-
-let monthValue = document.querySelector('.month-value');
-console.log(monthValue);
-
-let dayValue = document.querySelector('.day-value');
-console.log(dayValue);
-
-let chooseIncome = document.querySelector('.choose-income');
-console.log(chooseIncome);
-let sum = document.querySelector('.choose-sum');
-console.log(sum); 
-
-let percent = document.querySelector('.choose-percent');
-console.log(percent); 
-
-let checkBox = document.querySelector('#savings');
-console.log(checkBox); 
-
-
-//   Получить оставшиеся поля через querySelector (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
+    
+    expensesInput = document.getElementsByClassName('expenses-item'),
+    buttonApprove = document.getElementsByTagName('button')[0],
+    optionalExpensesButton = document.getElementsByTagName('button')[1],
+    countButton = document.getElementsByTagName('button')[2],
+    optionalExpenses = document.querySelectorAll('.optionalexpenses-item'),
+    yearInput = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value'),
+    chooseIncome = document.querySelector('.choose-income'),
+    sum = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    checkBox = document.querySelector('#savings');
